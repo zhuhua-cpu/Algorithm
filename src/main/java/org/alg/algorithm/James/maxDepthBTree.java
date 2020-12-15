7a69ceffb84e006f358fa8c0052a68697b76b11f
zhuhua <zhuhua@zuoyebang.com>
2020-12-16 00:45:03 +0800

    update

--- src/main/java/org/alg/algorithm/James/maxDepthBTree.java
+++ src/main/java/org/alg/algorithm/James/maxDepthBTree.java
@@ -12,6 +12,9 @@ class Solution {
       public int maxDepth(TreeNode root) {
         return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
-
+  public static void main(String[] args){
+    System.out.println("wodee");
+   
+}
 }
 ~
